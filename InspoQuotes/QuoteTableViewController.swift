1746ce0d987143c443af54a78884fc781767bed2
Tran L <52722842+trannble@users.noreply.github.com>
2020-04-17 10:39:31 -0700

    Complete Initital Setup

--- InspoQuotes/QuoteTableViewController.swift
+++ InspoQuotes/QuoteTableViewController.swift
@@ -40,15 +40,18 @@ class QuoteTableViewController: UITableViewController {
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return quotesToShow.count
     }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+           let cell = tableView.dequeueReusableCell(withIdentifier: "QuoteCell", for: indexPath)
+           cell.textLabel?.text = quotesToShow[indexPath.row]
+           cell.textLabel?.numberOfLines = 0 //take as many lines as needed
+           return cell
+       }
+    
 
     /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
